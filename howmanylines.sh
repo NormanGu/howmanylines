cd4c653d7cd8bd71bb374e3d374ac6d346a1079b
junqing.gu <junqing.gu@enflame-tech.com>
2021-02-10 15:45:07 +0800

    Add status basr support

--- howmanylines.sh
+++ howmanylines.sh
@@ -11,15 +11,33 @@ sum=0
 total=0
 
 
-##################################################
+###############################################################################################################################
+print_error()
+{
+	echo -e "\033[31m$1\033[0m"
+	exit 1
+}
+
+print_info()
+{
+	echo -e "\033[32m$1\033[0m"
+}
+
+print_warn()
+{
+	echo -e "\033[33m$1\033[0m"
+}
+
 usage()
 {
 	echo "howmanylines.sh [option] <type> <path>"
 	echo "                -s:  show summary"
 	echo "                -d:  show detail "
+	echo "example: "
+	echo "howmanylines.sh \"*.c\" /home/workspace/ "
 }
 
-print_info()
+info()
 {
 	echo "*************INFO***************"
 	echo "File type:    $ftype"
@@ -28,8 +46,39 @@ print_info()
 	echo "Show detail:  $show_detail"
 	echo "********************************"
 }
-##################################################
 
+delete_string()
+{
+	local len=$1
+	echo -ne "\r"
+	while ((len > 0))
+	do
+		echo -ne " "
+		let len--
+	done
+	echo -ne "\r"
+}
+
+cal()
+{
+	sum=0
+	local len=0
+	if [ "$show_summary" == "y" ] ; then
+		find $2 -name "$ftype" | xargs awk 'BEGIN{x=0} $0 !~ /^$/{x=x+1} END{print "sum:" " " x " " "lines"}'
+	else
+		f=$(find $2 -name "$ftype")
+		for i in $f ; do
+			len=${#i}
+			echo -ne "$i"
+			numlines=$(awk 'BEGIN{x=0} $0 !~ /^$/{x=x+1} END{print x}' $i)
+			sum=$(expr $numlines + $sum)
+			delete_string $len
+			#sum=$(awk -v x=$sum 'BEGIN{} $0 !~ /^$/{x=x+1} END{print x}' $i)
+		done
+	fi
+}
+
+#############################################################################################################################
 
 
 while getopts 'hsd' c; do
@@ -48,35 +97,20 @@ ftype=$1
 spath=$2
 
 
-cal()
-{
-sum=0
-
-if [ "$show_summary" == "y" ] ; then
-	find $2 -name "$ftype" | xargs awk 'BEGIN{x=0} $0 !~ /^$/{x=x+1} END{print "sum:" " " x " " "lines"}'
-else
-	f=$(find $2 -name "$ftype")
-	for i in $f ; do
-		numlines=$(awk 'BEGIN{x=0} $0 !~ /^$/{x=x+1} END{print x}' $i)
-		#echo "File: $i [$numlines]"
-		sum=$(expr $numlines + $sum)
-		#sum=$(awk -v x=$sum 'BEGIN{} $0 !~ /^$/{x=x+1} END{print x}' $i)
-	done
-	#echo $sum
-fi
-}
-
 all=$(ls $spath)
 for each in $all ; do
+	itype="File"
 	entry=$spath/$each
-	cal $ftype $entry
 	if [ -d "$entry" ]; then
-		printf "Dir  %20s : %s\n" "[$each]" "$sum"
-	else
-		if [ "$sum" -gt "0" ]; then
-			printf "File %20s : %s\n" "[$each]" "$sum"
-		fi
+		print_info "reading $entry"
+		itype="Dir"
 	fi
+	cal $ftype $entry
+
+	if [ "$sum" -gt "0" -o -d "$entry" ]; then
+		printf "$itype  %20s : %s\n" "[$each]" "$sum"
+	fi
+
 	total=$(expr $total + $sum)
 done
 
